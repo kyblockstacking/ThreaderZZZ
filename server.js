4742c1e15184d24ad966516fd10c0314d20ede6b
lookrumad <verniedelacruz@gmail.com>
2018-10-27 14:21:52 -0700

    login system still needs work; signup route complete

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+var bodyParser = require("body-parser");
+var session = require("express-session");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,12 +8,26 @@ const app = express();
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(session({
+  secret: "kevAidDavVer", 
+  resave: false,
+  saveUninitialized: true,
+  cookie: {secure: "auto", maxAge: 99999}
+}));
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 // Define API routes here
+
+const routes = require("./routes/api.js");
+app.use(routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
