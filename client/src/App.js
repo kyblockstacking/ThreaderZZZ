edcd9a26026073d769eeffd575e1a1d8ff29b0ac
kyblockstacking <kevin1992yang@gmail.com>
2018-10-30 17:11:05 -0700

    add 404 page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import Signup from './components/Signup';
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Jumbotron from "./components/Jumbotron";
 import Navbar from "./components/Navbar";
@@ -11,6 +11,7 @@ import PHPTreads from "./components/PHPForum";
 import PythonTreads from "./components/PythonForum";
 import Footer from "./components/Footer";
 import SignIn from "./components/SignIn";
+import NotFound from "./components/404";
 
 class App extends React.Component {
 
@@ -21,13 +22,15 @@ class App extends React.Component {
           <Jumbotron />
           <Navbar />
           <Route exact path="/" component={SignIn} />
-          <Route exact path="/" component={ForumCategory} />
-          {/* <Route exact path="/forum/:category" component={JavascriptTreads} /> */}
-          <Route exact path="/forum/Javascript" component={JavascriptTreads} />
-          <Route exact path="/forum/Javascript/thread=:id" component={JsThreadContent} />
-          <Route exact path="/forum/PHP" component={PHPTreads} />
-          <Route exact path="/forum/Python" component={PythonTreads} />
-          <Route exact path="/signup" component={Signup} />
+          <Switch>
+            <Route exact path="/" component={ForumCategory} />
+            <Route exact path="/forum/Javascript" component={JavascriptTreads} />
+            <Route exact path="/forum/Javascript/thread=:id" component={JsThreadContent} />
+            <Route exact path="/forum/PHP" component={PHPTreads} />
+            <Route exact path="/forum/Python" component={PythonTreads} />
+            <Route exact path="/signup" component={Signup} />
+            <Route component={NotFound} />
+          </Switch>
           <Footer />
         </div>
       </Router>
